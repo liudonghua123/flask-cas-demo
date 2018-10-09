17f09963da199127324e69374b1c3210b052e7c8
刘东华 <liudonghua@liudonghuas-MacBook-Air.local>
2018-10-09 20:42:59 +0800

    add forward functionalities

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, stream_with_context
 from flask_cas import CAS, login_required
 from flask_cas import login
 from flask_cas import logout
+import requests
 import logging
 from pprint import pprint
 from flask_debugtoolbar import DebugToolbarExtension
@@ -10,15 +11,15 @@ app = Flask(__name__)
 cas = CAS(app)
 
 # configure the cas of ynu
-# app.config['CAS_SERVER'] = 'http://ids.ynu.edu.cn'
-# app.config['CAS_LOGIN_ROUTE'] = '/authserver/login'
-# app.config['CAS_LOGOUT_ROUTE'] = '/authserver/logout'
-# app.config['CAS_VALIDATE_ROUTE'] = '/authserver/serviceValidate'
+app.config['CAS_SERVER'] = 'http://ids.ynu.edu.cn'
+app.config['CAS_LOGIN_ROUTE'] = '/authserver/login'
+app.config['CAS_LOGOUT_ROUTE'] = '/authserver/logout'
+app.config['CAS_VALIDATE_ROUTE'] = '/authserver/serviceValidate'
 # standard cas
-app.config['CAS_SERVER'] = 'http://localhost:8080'
-app.config['CAS_LOGIN_ROUTE'] = '/cas/login'
-app.config['CAS_LOGOUT_ROUTE'] = '/cas/logout'
-app.config['CAS_VALIDATE_ROUTE'] = '/cas/serviceValidate'
+# app.config['CAS_SERVER'] = 'http://localhost:8080'
+# app.config['CAS_LOGIN_ROUTE'] = '/cas/login'
+# app.config['CAS_LOGOUT_ROUTE'] = '/cas/logout'
+# app.config['CAS_VALIDATE_ROUTE'] = '/cas/serviceValidate'
 
 # configure some other settings
 app.config['CAS_AFTER_LOGIN'] = '/'
@@ -31,25 +32,14 @@ app.config['SECRET_KEY'] = 'some_secret_key_here'
 # https://pypi.org/project/Flask-DebugToolbar/
 toolbar = DebugToolbarExtension(app)
 
-    
-@app.route("/")
+@app.route("/protected/<path:url>")
 @login_required
-def index():
-    username = cas.username
-    return "This is protected index content of " + username
+def protected(url):
+    req = requests.get("http://github.com/{url}".format(url=url), stream = True)
+    return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
 
-@app.route("/protected/")
-@login_required
-def protected():
-    # import pdb; pdb.set_trace()
-    # pprint(vars(cas))
-    username = cas.username
-    # cas.attributes['myattr'] = 'myvalue'
-    # return "This is protected content of " + username
-    return render_template('protected.html', username = username, cas = cas)
+@app.errorhandler(404) 
+def not_found(e): 
+    return "404"
 
-@app.route("/public/")
-def public():
-    return "This is public content!"
-
-app.run(host='0.0.0.0', port=9000, debug=True)
+app.run(host='0.0.0.0', port=8080, debug=True)
