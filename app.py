19b519a494ff4fdf4bfce4d7cf52298abf9dbffc
liudonghua123 <liudonghua123@gmail.com>
2026-03-31 16:54:12 +0800

    update code

--- app.py
+++ app.py
@@ -4,21 +4,33 @@ from flask_cas import login
 from flask_cas import logout
 import logging
 from pprint import pprint
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask.globals import _request_ctx_stack
+# ImportError: cannot import name '_request_ctx_stack' from 'flask.globals'
+# from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 cas = CAS(app)
 
 # configure the cas of ynu
-# app.config['CAS_SERVER'] = 'http://ids.ynu.edu.cn'
-# app.config['CAS_LOGIN_ROUTE'] = '/authserver/login'
-# app.config['CAS_LOGOUT_ROUTE'] = '/authserver/logout'
-# app.config['CAS_VALIDATE_ROUTE'] = '/authserver/serviceValidate'
+# app.config['CAS_SERVER'] = 'https://cas.ynou.edu.cn'
+# app.config['CAS_LOGIN_ROUTE'] = '/cas/login'
+# app.config['CAS_LOGOUT_ROUTE'] = '/cas/logout'
+# # app.config['CAS_VALIDATE_ROUTE'] = '/cas/serviceValidate'
+# app.config['CAS_VALIDATE_ROUTE'] = '/cas/p3/serviceValidate'
+
 # standard cas
-app.config['CAS_SERVER'] = 'http://localhost:8080'
-app.config['CAS_LOGIN_ROUTE'] = '/cas/login'
-app.config['CAS_LOGOUT_ROUTE'] = '/cas/logout'
-app.config['CAS_VALIDATE_ROUTE'] = '/cas/serviceValidate'
+# app.config['CAS_SERVER'] = 'http://localhost:8080'
+# app.config['CAS_LOGIN_ROUTE'] = '/cas/login'
+# app.config['CAS_LOGOUT_ROUTE'] = '/cas/logout'
+# # app.config['CAS_VALIDATE_ROUTE'] = '/cas/serviceValidate'
+# app.config['CAS_VALIDATE_ROUTE'] = '/cas/p3/serviceValidate'
+
+# ynu ids cas
+app.config['CAS_SERVER'] = 'https://ids.ynu.edu.cn'
+app.config['CAS_LOGIN_ROUTE'] = '/authserver/login'
+app.config['CAS_LOGOUT_ROUTE'] = '/authserver/logout'
+# app.config['CAS_VALIDATE_ROUTE'] = '/authserver/serviceValidate'
+app.config['CAS_VALIDATE_ROUTE'] = '/authserver/p3/serviceValidate'
 
 # configure some other settings
 app.config['CAS_AFTER_LOGIN'] = '/'
@@ -29,7 +41,7 @@ app.config['SECRET_KEY'] = 'some_secret_key_here'
 # # the toolbar is only enabled in debug mode:
 # app.debug = True
 # https://pypi.org/project/Flask-DebugToolbar/
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
     
 @app.route("/")
@@ -52,4 +64,4 @@ def protected():
 def public():
     return "This is public content!"
 
-app.run(host='0.0.0.0', port=9000, debug=True)
+app.run(host='0.0.0.0', port=8080, debug=True)
